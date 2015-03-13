dc5c80dc344672c6bfd56ce1088bf8aa6173bfdd
Rafael Neri <rafael.neri@gmail.com>
2015-03-12 23:11:46 -0300

    Loop to test

--- src/hx711.js
+++ src/hx711.js
@@ -1,5 +1,8 @@
-var mraa = require('mraa');
 var hx711 = require('./build/Release/hx711');
 
 var scale = new hx711.HX711(3,2);
-console.log(scale.getValue());
+
+for(var i=0; i<100; i++)
+{
+	console.log(scale.getValue());
+}
