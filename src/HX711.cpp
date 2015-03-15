890fce590a03e4803f616d30615c1dad646424c8
Rafael Neri <rafael.neri@gmail.com>
2015-03-15 20:51:11 -0300

    Revert "Fix 16777215 ADC read"
    This reverts commit 7c0dd82129ce239ee6cca58d6148999d9fa16277.

--- src/HX711.cpp
+++ src/HX711.cpp
@@ -89,12 +89,7 @@ Handle<Value> HX711::GetValue(const Arguments& args) {
     HandleScope scope;
     
     HX711* obj = ObjectWrap::Unwrap<HX711>(args.This());
-    unsigned long result = 0;
-    
-    do
-    {
-        result = obj->adcRead();
-    }while(!(result ^ 0xffffff));
+    unsigned long result = obj->adcRead();
     
     return scope.Close(Number::New(result));
 }
